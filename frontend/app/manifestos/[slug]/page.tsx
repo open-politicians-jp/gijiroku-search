21d02e09af6aa1573ce96c0ae60bff7936016bca
hironeko <hironeko.sun@gmail.com>
2026-01-31 01:32:05 +0900

    fix

--- frontend/app/manifestos/[slug]/page.tsx
+++ frontend/app/manifestos/[slug]/page.tsx
@@ -4,6 +4,36 @@ interface ManifestoSlugPageProps {
   params: { slug: string };
 }
 
+const MANIFESTO_SLUGS = [
+  'genzei-nihon',
+  'jiyuminshuto',
+  'kakuyugo-to',
+  'kokka-seishin-kai',
+  'kokuminminshuto',
+  'kokusei-governance',
+  'komeito',
+  'kyosanto',
+  'nhk-to',
+  'nihon-hoshu-to',
+  'nihon-kaikaku-to',
+  'nihon-katei-mamoru-kai',
+  'nipponishin',
+  'reiwa',
+  'rikkenminshuto',
+  'saisei-no-michi',
+  'sanseito',
+  'shakai-minshu-to',
+  'shinto-kunimori',
+  'shinto-yamato',
+  'team-mirai',
+  'teammirai',
+  'zeikin-tomei-ka-to'
+];
+
+export function generateStaticParams() {
+  return MANIFESTO_SLUGS.map((slug) => ({ slug }));
+}
+
 export default function ManifestoSlugPage({ params }: ManifestoSlugPageProps) {
   redirect(`/manifestos/llm/${params.slug}`);
 }
